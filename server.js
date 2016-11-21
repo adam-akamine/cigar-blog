3e6157f48a1086f7cada8bd2270fa3df14ef6546
kekuso <kekuso808@gmail.com>
2016-11-20 21:56:31 -1000

    Added cloudinary uploading in server.js

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var pug = require('pug');
 var path = require('path');
 var express = require('express');
 var multiparty = require('multiparty');
+var cloudinary = require('cloudinary');
+var cloudConfig = require('./config/cloudConfig');
 
 var app = express();
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+
+cloudinary.config({
+  cloud_name: cloudConfig.name,
+  api_key: cloudConfig.key,
+  api_secret: cloudConfig.secret
+});
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -22,7 +30,19 @@ app.get('/reviews/new', function(req, res) {
 })
 
 app.post('/reviews', function(req, res) {
+  var form = new multiparty.Form();
+  form.parse(req, function(err, fields, files) {
+    if(err)
+      throw err;
+    if(files.pic[0].size) {
+      cloudinary.uploader.upload(files.pic[0].path, function (result) {
+        return Pics.create({fileName: result.url})
+        .then(function(cloudPic) {
 
+        })
+      })
+    }
+  })
 })
 
 var server = app.listen(3000, function () {
